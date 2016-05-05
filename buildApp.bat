399a7d908e998512e35372a0d98cbc8b17d431f8
Gaurang Patel <meetgaurang@gmail.com>
2016-05-06 01:44:25 +0530

    Added all Jenkins command in a batch file.. So that it can be version controlled..

--- buildApp.bat
+++ buildApp.bat
@@ -1,3 +1,6 @@
+cd appawesome
+call npm install
+call grunt
 rmdir /s /Q appawesome-cordova-temp
 call cordova create appawesome-cordova-temp com.gaurang.patel AppAwesome
 rmdir /s /Q appawesome-cordova-temp\www
