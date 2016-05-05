d7aaf48174c4a61e9e7230ae91531c707e3891f1
Gaurang Patel <meetgaurang@gmail.com>
2016-05-06 01:34:45 +0530

    Scripts to automate APK building on Jenkins..

---
+++ buildApp.bat
@@ -0,0 +1,8 @@
+rmdir /s /Q appawesome-cordova-temp
+call cordova create appawesome-cordova-temp com.gaurang.patel AppAwesome
+rmdir /s /Q appawesome-cordova-temp\www
+xcopy /E /Y appawesome-cordova appawesome-cordova-temp
+cd appawesome-cordova-temp
+call cordova platform add android
+call cordova build
+cd ..
